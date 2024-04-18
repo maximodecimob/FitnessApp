7438b8415d5189ab9e4f0a31094ed81cbf6bf517
MenciaMH <163448339+MenciaMH@users.noreply.github.com>
2024-04-18 10:33:47 +0200

    Créditos en el Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,18 @@
+/*
+Titulo: Práctica 2 - FitnessApp
+
+Desarrollado por:
+Equipo 2 INF-A
+
+Créditos:
+Jacobo Bascon Montalvo
+Mencía Marzal Higuero
+Dylan Novey Pérez
+Eduardo Estefanía Ovejero
+
+Fecha de finalización: 18/04/2024
+ */
+
 public class Main {
     public static void main(String[] args) {
         ProgramaPrincipal.menuGeneral();
