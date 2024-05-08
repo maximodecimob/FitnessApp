daec157c809461e8a3806f08933b6994c8e187d2
maximodecimob <jacobobascon@gamil.com>
2024-05-08 17:21:14 +0200

    Mejoras en el programa hasta el 6

--- src/Excepciones/ConjuntoVacioException.java
+++ src/Excepciones/ConjuntoVacioException.java
@@ -1,9 +1,6 @@
 package Excepciones;
 
 public class ConjuntoVacioException extends Exception{
-    public ConjuntoVacioException() {
-    }
-
     public ConjuntoVacioException(String message) {
         super(message);
     }
